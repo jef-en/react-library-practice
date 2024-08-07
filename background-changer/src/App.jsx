7c04cefd2c6ac2cf2adc13c76febc0252e68b956
Jefen De Villa <jedevilla@gbox.adnu.edu.ph>
2024-08-07 11:56:02 +0800

    initial commit

---
+++ background-changer/src/App.jsx
@@ -0,0 +1,10 @@
+import ColorPicker from "./components/ColorPicker";
+
+function App() {
+  return (
+    <>
+      <ColorPicker />
+    </>
+  );
+}
+export default App;
---
+++ background-changer/src/components/ColorPicker.jsx
@@ -0,0 +1,4 @@
+function ColorPicker() {
+  return <div className="main-container"></div>;
+}
+export default ColorPicker;
