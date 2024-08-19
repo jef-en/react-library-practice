d23c46c034a34cb37bb95b291238f3af8721f791
Jefen De Villa <jedevilla@gbox.adnu.edu.ph>
2024-08-19 18:01:05 +0800

    Continue on CSS

--- todo-list/src/components/ToDoList.jsx
+++ todo-list/src/components/ToDoList.jsx
@@ -10,16 +10,22 @@ function ToDoList() {
         "Buy groceries for the week including vegetables, fruits, milk, and bread.",
     },
     {
-      title: "Finish Project Report",
+      title: "Scheduled Check-up",
       id: 2,
+      content:
+        "Call the clinic to schedule a check-up appointment for next week and confirm the insurance details.",
+    },
+    {
+      title: "Finish Project Report",
+      id: 3,
       content:
         "Complete the final draft of the project report and send it to the team for review.",
     },
     {
-      title: "Schedule Doctor's Appointment",
-      id: 3,
+      title: "Book Flight Tickets",
+      id: 4,
       content:
-        "Call the clinic to schedule a check-up appointment for next week and confirm the insurance details.",
+        "Search for and book flight tickets for the upcoming conference in December. Compare options and prices.",
     },
   ]);
   const [newTask, setNewTask] = useState({
@@ -39,7 +45,7 @@ function ToDoList() {
     const { title, content } = newTask;
     if (title.trim() && content.trim()) {
       setTask([...task, { id: uuidv4(), isComplete: false, ...newTask }]); // tried using the uuid
-      setNewTask({ title: "", content: "" });
+      // setNewTask({ title: "", content: "" });
     } else {
       alert("Please fill up all fields");
       setNewTask({ title: "", content: "" });
@@ -76,25 +82,41 @@ function ToDoList() {
           value={newTask.content}
           onChange={handleChange}
         />
-        <button onClick={handleAdd}>Add Another Task</button>
+        <button onClick={handleAdd} className="btn b">
+          Add Another Task
+        </button>
       </div>
-
+      <div id="productivity-container">
+        <h1>dark mode </h1>
+        <h1>Time</h1>
+      </div>
       <div id="task-container">
         {task.map((value) => (
-          <div
-            id="task"
-            key={value.id}
-            style={{
-              textDecoration: value.isComplete ? "line-through" : "none",
-            }}
-          >
-            <h3>{value.title}</h3>
-            <p>{value.content}</p>
+          <div id="task" key={value.id}>
+            <h3
+              className="task-title"
+              style={{
+                textDecoration: value.isComplete ? "line-through" : "none",
+              }}
+            >
+              {value.title}
+            </h3>
+            <hr className="line" />
+            <p
+              className="task-content"
+              style={{
+                textDecoration: value.isComplete ? "line-through" : "none",
+              }}
+            >
+              {value.content}
+            </p>
             <div id="buttons">
-              <button onClick={() => handleComplete(value.id)}>
+              <button onClick={() => handleComplete(value.id)} className="btn">
                 {value.isComplete ? "Not Done" : "Done"}
               </button>
-              <button onClick={() => handleDelete(value.id)}>Delete</button>
+              <button onClick={() => handleDelete(value.id)} className="btn">
+                Delete
+              </button>
             </div>
           </div>
         ))}
